a7b722ceda8d24495d2ee66f85be33b3aecf072f
201213225 <kandor95@gmail.com>
2015-02-20 01:25:15 -0600

    estructura

---
+++ JavaApplication5/src/javaapplication5/Nodo_Matriz.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author Denis
+ */
+public class Nodo_Matriz {
+    Nodo_Matriz izq;
+    Nodo_Matriz der;
+    Nodo_Matriz ar;
+    Nodo_Matriz ab;
+}
